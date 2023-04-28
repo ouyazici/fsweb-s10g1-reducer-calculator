8d889db07a0266ff422f639dd6d4624f3ab7f478
ouyazici <onurulas_57@hotmail.com>
2023-04-28 17:29:12 +0300

    Gorev Tamamlandı

--- src/actions/index.js
+++ src/actions/index.js
@@ -2,11 +2,34 @@ export const ADD_ONE = "ADD_ONE";
 
 export const APPLY_NUMBER = "APPLY_NUMBER";
 export const CHANGE_OPERATION = "CHANGE_OPERATION";
+export const CLEAR_DISPLAY = "CLEAR_DISPLAY";
+export const MEMORY_ADD = "MEMORY_ADD";
+export const MR_ADD = "MR_ADD";
+export const MC_ZERO = "MC_ZERO";
+
+export const clearDisplay = () => {
+  return { type: CLEAR_DISPLAY };
+};
 
 export const addOne = () => {
-  return ({ type: ADD_ONE });
-}
+  return { type: ADD_ONE };
+};
 
 export const applyNumber = (number) => {
-  return ({ type: APPLY_NUMBER, payload: number });
-}
+  return { type: APPLY_NUMBER, payload: number };
+};
+
+export const changeOperation = (operation) => {
+  return { type: CHANGE_OPERATION, payload: operation };
+};
+
+export const memoryAdd = () => {
+  return { type: MEMORY_ADD };
+};
+export const mrAdd = () => {
+  return { type: MR_ADD };
+};
+
+export const mcZero = () => {
+  return { type: MC_ZERO };
+};
--- src/reducers/index.js
+++ src/reducers/index.js
@@ -1,47 +1,76 @@
-import { ADD_ONE, APPLY_NUMBER, CHANGE_OPERATION } from './../actions';
+import {
+  ADD_ONE,
+  APPLY_NUMBER,
+  CHANGE_OPERATION,
+  CLEAR_DISPLAY,
+  MEMORY_ADD,
+  MR_ADD,
+  MC_ZERO,
+} from "./../actions";
 
 export const initialState = {
-  total: 100,
+  total: 0,
   operation: "*",
-  memory: 100
-}
+  memory: 0,
+};
 
 const calculateResult = (num1, num2, operation) => {
   switch (operation) {
-    case ("+"):
+    case "+":
       return num1 + num2;
-    case ("*"):
+    case "*":
       return num1 * num2;
-    case ("-"):
+    case "-":
       return num1 - num2;
     default:
       return;
   }
-}
+};
 
-const reducer = (state, action) => {
+const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case (ADD_ONE):
-      return ({
+    case ADD_ONE:
+      return {
         ...state,
-        total: state.total + 1
-      });
+        total: state.total + 1,
+      };
 
-    case (APPLY_NUMBER):
-      return ({
+    case APPLY_NUMBER:
+      return {
         ...state,
-        total: calculateResult(state.total, action.payload, state.operation)
-      });
+        total: calculateResult(state.total, action.payload, state.operation),
+      };
 
-    case (CHANGE_OPERATION):
-      return ({
+    case CHANGE_OPERATION:
+      return {
         ...state,
-        operation: action.payload
-      });
+        operation: action.payload,
+      };
+
+    case CLEAR_DISPLAY:
+      return {
+        ...state,
+        total: 0,
+      };
+    case MEMORY_ADD:
+      return {
+        ...state,
+        memory: state.total,
+      };
+    case MR_ADD:
+      return {
+        ...state,
+        total: state.memory,
+      };
+    case MC_ZERO:
+      return {
+        ...state,
+        memory: 0,
+      };
 
     default:
       return state;
   }
-}
+};
 
 export default reducer;
